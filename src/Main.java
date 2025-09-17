6f07abd3507769fb48f912c93575ca348f419cee
Marie23560 <kanzayiremariepascale@gmail.com>
2025-09-17 13:36:37 +0200

    changed number

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Choose a Shape: 1 = Circle,2 = Rectangle, 3 = Triangle");
+        System.out.println("Choose a Shape: 1 = Circle,2 = Rectangle, 4 = Triangle");
 
     }
 }
