194e3b0f85582abae2fee7c45a880c265d7dcbfc
Marie23560 <kanzayiremariepascale@gmail.com>
2025-09-17 18:38:17 +0200

    add length and width to find area of a rectangle

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Choose a Shape: 1 = Circle,2 = Rectangle, 4 = Triangle");
+        System.out.println("Choose a Shape: 1 = Circle,2 = Rectangle, 3 = Triangle");
+        int choice = sc.nextByte();
+
+        Absshape shape ;
+
+        switch (choice){
+            case 1:
+                System.out.println("Enter radius: ");
+                double r = sc.nextByte();
+                shape =new AbsCircle(r);
+                break;
+        }
+
 
     }
 }
